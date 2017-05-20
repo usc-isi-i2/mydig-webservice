dedf671e91af1c5a6768de4b4f6de5bd68152d79
Amandeep Singh <amandeep.s.saggu@gmail.com>
2017-05-19 18:01:20 -0700

    add es module, load tags to es

--- ws/config.py
+++ ws/config.py
@@ -7,11 +7,15 @@ config = {
         'port': 5000,
     },
     'repo': {
-        'local_path': '../../mydig-projects-test',
+        'local_path': '../../mydig-projects',
     },
     'logging': {
         'file_path': 'log.log',
         'format': '%(asctime)s %(levelname)s %(message)s',
         'level': logging.INFO
+    },
+    'write_es': {
+        'index': 'dig-sample-sample',
+        'es_url': 'http://some_valid_url:9200'
     }
 }
