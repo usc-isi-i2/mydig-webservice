80ca2b54e5ac1cfc4b4f27ecc986a1865f99dd1f
Amandeep Singh <amandeep.s.saggu@gmail.com>
2017-05-26 14:46:55 -0700

    update test

--- tests/test_tags_kg_id.py
+++ tests/test_tags_kg_id.py
@@ -29,12 +29,14 @@ class FlaskrTestCase(unittest.TestCase):
                                 "elastic_search_doctype": "the type in elastic search", "elastic_search_query": {},
                                 "start_date": "date-in-iso-format-at-any-resolution",
                                 "end_date": "date-in-iso-format-at-any-resolution"}]
+        print json.dumps(req_data)
         response = self.app.post('/projects', data=json.dumps(req_data))
         req_data = dict()
         req_data['human_annotation'] = 0
         req_data['tags'] = 'test-tag'
         response = self.app.post("/projects/dig3-ht/entities/092F55350A6125D8550D7652F867EBB9EB027C8EADA2CC1BAC0BEB1F48FE6D2B/tags", data=json.dumps(req_data))
         req_data['human_annotation'] = 1
+        print json.dumps(req_data)
         response = self.app.post(
             "/projects/dig3-ht/entities/CAFAE7C3F6B4A45A6ADB342A8C09051E34DDE45D4ECD7A9620BDFFCE55702C58/tags",
             data=json.dumps(req_data))
