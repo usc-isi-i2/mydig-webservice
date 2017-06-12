e31a903fd47764802362353f0fbbfe6bc09f41a0
greatyyx <bigyyx@gmail.com>
2017-06-12 16:07:06 -0700

    add git operations

--- ws/config.py
+++ ws/config.py
@@ -8,6 +8,16 @@ config = {
     },
     'repo': {
         'local_path': '../../mydig-projects-test',
+        # make sure remote alias had been created
+        # https://help.github.com/articles/changing-a-remote-s-url/
+        # git remote set-url origin https://github.com/{owner}/{repo}.git
+        # and also generate & deploy ssh key
+        # https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/
+        'github': {
+            'ssh_key_file_path': '<file path>',
+            # 'username': 'usc-isi-i2',
+            # 'access_token': '<Access Token>'
+        }
     },
     'logging': {
         'file_path': 'log.log',
@@ -15,7 +25,6 @@ config = {
         'level': logging.INFO
     },
     'es': {
-        # http://10.1.94.103:9201
         # do not add / at the end
         'url': 'http://localhost:9200'
     },
