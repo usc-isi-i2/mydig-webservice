1cd570a90cf92815a48a0833e27d6130d4b69154
greatyyx <bigyyx@gmail.com>
2017-06-25 11:27:15 -0700

    update bugs in restart script

--- frontend/frontend.sh
+++ frontend/frontend.sh
@@ -3,4 +3,4 @@
 echo "killing frontend process"
 ps -ef | grep "dummy-this-is-mydig-frontend" | awk '{print $2}' | xargs kill -9
 echo "restart frontend"
-./nohup python service.py --dummy-this-is-mydig-frontend &
+nohup python service.py --dummy-this-is-mydig-frontend &
--- ws/backend.sh
+++ ws/backend.sh
@@ -3,4 +3,4 @@
 echo "killing backend process"
 ps -ef | grep "dummy-this-is-mydig-backend" | awk '{print $2}' | xargs kill -9
 echo "restart backend"
-./nohup python ws.py --dummy-this-is-mydig-backend &
+nohup python ws.py --dummy-this-is-mydig-backend &
