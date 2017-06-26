670a363871a7f189da56c2259dfa1e20820571c1
greatyyx <bigyyx@gmail.com>
2017-06-26 11:34:23 -0700

    update bugs in frontend script

--- frontend/frontend.sh
+++ frontend/frontend.sh
@@ -5,8 +5,9 @@ restart=${1:-yes}
 echo "killing frontend process (if exists)"
 ps -ef | grep "dummy-this-is-mydig-frontend" | awk '{print $2}' | xargs kill -9
 
-
-if [ ${restart} != "yes" ]; then
+if [ ${restart} != "no" ]; then
     echo "starting frontend"
     nohup python service.py --dummy-this-is-mydig-frontend &
 fi
+
+echo "done"
