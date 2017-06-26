f5417eb82d35284d56f02cecc5a8ad1a50a8bd50
greatyyx <bigyyx@gmail.com>
2017-06-26 11:32:07 -0700

    update scripts

--- frontend/frontend.sh
+++ frontend/frontend.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-restart=${1:-no}
+restart=${1:-yes}
 
 echo "killing frontend process (if exists)"
 ps -ef | grep "dummy-this-is-mydig-frontend" | awk '{print $2}' | xargs kill -9
 
 
-if [ ${restart} -eq no ]; then
+if [ ${restart} != "yes" ]; then
     echo "starting frontend"
     nohup python service.py --dummy-this-is-mydig-frontend &
 fi
--- ws/backend.sh
+++ ws/backend.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-restart=${1:-no}
+restart=${1:-yes}
 
 echo "killing backend process (if exists)"
 ps -ef | grep "dummy-this-is-mydig-backend" | awk '{print $2}' | xargs kill -9
 
-if [ ${restart} -eq no ]; then
+if [ ${restart} != "no" ]; then
     echo "starting backend"
     nohup python ws.py --dummy-this-is-mydig-backend &
 fi
