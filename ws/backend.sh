c2269564eabca7f44972dd3694db522050352778
greatyyx <bigyyx@gmail.com>
2017-07-06 14:31:16 -0700

    update backend.sh to support killing subprocesses

--- ws/backend.sh
+++ ws/backend.sh
@@ -4,6 +4,7 @@ restart=${1:-yes}
 
 echo "killing backend process (if exists)"
 ps -ef | grep "dummy-this-is-mydig-backend" | awk '{print $2}' | xargs kill -9
+ps -ef | grep "dummy-this-is-mydig-backend-etk-process" | awk '{print $2}' | xargs kill -9
 
 if [ ${restart} != "no" ]; then
     echo "starting backend"
--- ws/run_etk.sh
+++ ws/run_etk.sh
@@ -6,12 +6,14 @@ conda_bin_path="$3"
 etk_path="$4"
 num_processes="$5"
 
+# prepare data source
 #data_path="${working_dir}/etk_input.jl"
 #if [ ! -f ${data_path} ]; then
     data_path="${working_dir}/consolidated_data.jl"
     cat ${page_path}/* > ${data_path}
 #fi
 
+# initiate etk env
 source ${conda_bin_path}/activate etk_env
 
 # serial
@@ -21,12 +23,16 @@ source ${conda_bin_path}/activate etk_env
 #    -c ${working_dir}/etk_config.json > ${working_dir}/etk_stdout.txt
 
 
-# parallel
+# create output tmp dir
 if [ ! -d ${working_dir}/tmp ]; then
     mkdir ${working_dir}/tmp
 fi
 
+# clean previous outputs and progress
 rm ${working_dir}/tmp/output_chunk_*
+rm ${working_dir}/etk_progress
+
+# create progress file
 num_of_docs=$(wc -l ${working_dir}/consolidated_data.jl | awk '{print $1}')
 while true; do sleep 5; \
     wc -l ${working_dir}/tmp/output_chunk_* | tail -n 1 | awk -v total=$num_of_docs '{print total" "$1}' \
@@ -34,19 +40,23 @@ while true; do sleep 5; \
 done &
 progress_job_id=$!
 
+# run etk parallelly
 python ${etk_path}/etk/run_core.py \
     -i ${data_path} \
     -o ${working_dir}/tmp \
+    --dummy-this-is-mydig-backend-etk-process
     -c ${working_dir}/etk_config.json \
     -m -t ${num_processes} > ${working_dir}/etk_stdout.txt
 last_exit_code=$?
 
+# close progress background job
 kill ${progress_job_id}
 
 if [ ${last_exit_code} == 0 ]; then
     exit ${last_exit_code}
 fi
 
+# concatenate outputs
 cat ${working_dir}/tmp/* > ${working_dir}/etk_out.jl
 
 #if [ ${last_exit_code} == 0 ]; then
