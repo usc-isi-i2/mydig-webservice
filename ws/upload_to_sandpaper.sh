985aaed3d544c87f704d69da517a233cfab4bf41
greatyyx <bigyyx@gmail.com>
2017-07-07 21:02:58 -0700

    fix bugs in upload_to_sandpaper script

--- ws/upload_to_sandpaper.sh
+++ ws/upload_to_sandpaper.sh
@@ -8,13 +8,15 @@ type="$5"
 working_dir="$6"
 
 
-status_code=$(curl -LI -XPUT "{$sandpaper_url}/mapping?url=${ws_url}&project=${project_name}&index=${index}" \
-    -o /dev/null -w '%{http_code}\n' -s)
-if [ ${status_code} -ne 200 ]; then
+status_code=$(curl -s -o /dev/null -w "%{http_code}"  \
+    -XPUT "{$sandpaper_url}/mapping?url=${ws_url}&project=${project_name}&index=${index}")
+if [ $(( ${status_code}/100 )) -ne 2 ]; then
     exit ${status_code}
 fi
 
-status_code=$(curl -LI -H "Content-Type: application/json" -XPOST \
+status_code=$(curl -s -o /dev/null -w "%{http_code}" \
+    -H "Content-Type: application/json" -XPOST \
     --data-binary "@${working_dir}/etk_out.jl" \
-    "{$sandpaper_url}/indexing?index=${index}" -o /dev/null -w '%{http_code}\n' -s)
+    "${sandpaper_url}/indexing?index=${index}" \
+    -o /dev/null -w '%{http_code}\n' -s)
 exit ${status_code}
