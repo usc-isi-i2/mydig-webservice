05051bc91a50929b4f589d5dd5c4a3c3f89dcaf1
greatyyx <bigyyx@gmail.com>
2017-07-10 18:06:46 -0700

    add timeout for the request of inferlink tool

--- ws/ws.py
+++ ws/ws.py
@@ -1910,7 +1910,7 @@ class Actions(Resource):
             logger.info('sent to inferlink: url: {} payload: {}'.format(url, json.dumps(payload)))
             payload_dump_path = os.path.join(_get_project_dir_path(project_name), 'working_dir/last_payload.json')
             write_to_file(json.dumps(payload, indent=2), payload_dump_path)
-            resp = requests.post(url, json.dumps(payload))
+            resp = requests.post(url, json.dumps(payload), timeout=10)
             if resp.status_code // 100 != 2:
                 logger.error('invoke inferlink server {}: {}'.format(url, resp.content))
 
