8197076446d9846d79c821af8a4708197f95c915
greatyyx <bigyyx@gmail.com>
2017-07-10 20:37:47 -0700

    update error handling for request of inferlink

--- ws/ws.py
+++ ws/ws.py
@@ -1910,9 +1910,12 @@ class Actions(Resource):
             logger.info('sent to inferlink: url: {} payload: {}'.format(url, json.dumps(payload)))
             payload_dump_path = os.path.join(_get_project_dir_path(project_name), 'working_dir/last_payload.json')
             write_to_file(json.dumps(payload, indent=2), payload_dump_path)
-            resp = requests.post(url, json.dumps(payload), timeout=10)
-            if resp.status_code // 100 != 2:
-                logger.error('invoke inferlink server {}: {}'.format(url, resp.content))
+            try:
+                resp = requests.post(url, json.dumps(payload), timeout=10)
+                if resp.status_code // 100 != 2:
+                    logger.error('invoke inferlink server {}: {}'.format(url, resp.content))
+            except requests.exceptions.Timeout as e:
+                logger.error('inferlink server timeout: {}'.format(e.message))
 
         if os.path.exists(lock_path):
             os.remove(lock_path)
