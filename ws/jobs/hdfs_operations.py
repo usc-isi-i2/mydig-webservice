1b21d5a1b921daafd73ca0e3cd29eb5d3c8ff8e9
Amandeep Singh <amandeep.s.saggu@gmail.com>
2017-07-12 09:37:40 -0400

    PEP 8

--- ws/jobs/hdfs_operations.py
+++ ws/jobs/hdfs_operations.py
@@ -1,7 +1,5 @@
 import requests
-from requests.auth import HTTPBasicAuth
 from pywebhdfs.webhdfs import PyWebHdfsClient
-import json
 
 
 class HdfsOp(object):
@@ -10,16 +8,17 @@ class HdfsOp(object):
 
     def create_dir(self, path, hdfs_user='worker'):
         url = '{}{}/?op=MKDIRS&user.name={}'.format(self.web_hdfs_url, path, hdfs_user)
-        print url
         return requests.put(url)
 
     def create_or_overwrite_file(self, path, f, hdfs_user='worker'):
         hdfs = PyWebHdfsClient(host='10.1.94.54', port=14000, user_name=hdfs_user)
         return hdfs.create_file(path, f, overwrite=True)
 
+
 if __name__ == '__main__':
     ho = HdfsOp()
     # res = ho.create_dir('/user/worker/DOTHIS')
     path = '/user/worker/DOTHIS/test.md'
     import codecs
+
     ho.create_or_overwrite_file(path, None)
