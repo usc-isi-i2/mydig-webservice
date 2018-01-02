b017557791e705943939f638ce1386014defdcac
greatyyx <bigyyx@gmail.com>
2018-01-02 11:46:16 -0800

    add sync to force synchronizing fs for chmod

--- docker_run_mydig.sh
+++ docker_run_mydig.sh
@@ -6,22 +6,25 @@ export PS1="\u:\W\$ "
 #source activate etk_env
 cd /app/mydig-webservice/daemon
 chmod +x daemon.sh
+sync
 ./daemon.sh
 #source deactivate
 
 # backend
 cd /app/mydig-webservice/ws
 chmod +x backend.sh
+sync
 ./backend.sh
 
 # frontend
 cd /app/mydig-webservice/frontend
 chmod +x frontend.sh
+sync
 ./frontend.sh
 
 # spacy_ui
 cd /app/spacy-ui
-nohup /usr/bin/serve -s build -p 9881 &
+/usr/bin/serve -s build -p 9881 &
 
 # open bash and wait
 #cd /app
