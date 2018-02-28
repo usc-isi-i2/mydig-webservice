12600ee3f3636bacbba92bea143a16090d7834e7
greatyyx <bigyyx@gmail.com>
2018-02-27 18:21:35 -0800

    create external crawler consumer

--- daemon/daemon.sh
+++ daemon/daemon.sh
@@ -13,6 +13,9 @@ if [ ${restart} != "no" ]; then
 #    python -u ache_consumer.py --dummy-this-is-mydig-daemon &
     echo "starting daemon (RSS Feed Crawler)"
     python -u rss_consumer.py --dummy-this-is-mydig-daemon &
+
+    echo "starting daemon (External Crawler)"
+    python -u external_crawler_consumer.py --dummy-this-is-mydig-daemon &
 fi
 
 echo "done"
