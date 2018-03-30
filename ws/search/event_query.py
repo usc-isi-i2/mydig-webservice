abcc2cbabb5f10c319da309e9f4a274caff4159c
Amandeep Singh <amandeep.s.saggu@gmail.com>
2018-03-29 17:02:21 -0700

    PEP8 compatible

--- ws/search/event_query.py
+++ ws/search/event_query.py
@@ -1,15 +1,14 @@
-import requests
 import rest
 import urllib
-import traceback,sys
 from conjunctive_query import ConjunctiveQueryProcessor
-from response_converter import DigOutputProcessor,TimeSeries
-import json,sys,traceback
+from response_converter import DigOutputProcessor, TimeSeries
 import logging
 
 logger = logging.getLogger('mydig-webservice.log')
+
+
 class EventQueryProcessor(object):
-    def __init__(self,request,project_name,config_fields,project_root_name,es):
+    def __init__(self, request, project_name, config_fields, project_root_name, es):
         self.request = request
         self.preprocess()
         self.config = config_fields
@@ -17,11 +16,11 @@ class EventQueryProcessor(object):
         self.project_root_name = project_root_name
         self.project_name = project_name
         self.cquery = ConjunctiveQueryProcessor(self.request, self.project_name,
-                                          self.config,
-                                          self.project_root_name, self.es)
-        self.field = request.args.get('_group-by',"event_date")
-        self.agg = request.args.get('_interval',"month")
-        self.agg_field = request.args.get('_aggregation-field',None)
+                                                self.config,
+                                                self.project_root_name, self.es)
+        self.field = request.args.get('_group-by', "event_date")
+        self.agg = request.args.get('_interval', "month")
+        self.agg_field = request.args.get('_aggregation-field', None)
         if self.agg_field is not None and "." in self.agg_field:
             self.agg_field = self.convert_to_nested_field(self.agg_field)
         if self.field is not None and "." in self.field:
@@ -32,20 +31,21 @@ class EventQueryProcessor(object):
             arg = urllib.unquote(arg)
         return
 
-    def convert_to_nested_field(self,field):
+    def convert_to_nested_field(self, field):
         params = field.split('.')
         return params[0] + "__" + params[1]
 
     def process_ts_query(self):
-        '''
+        """
         This is the main function in this class. This calls several functions to validate input, set match clauses, set filter clauses
         set sort clauses and finally resolve any nested documents if needed. Finally this function returns the data as a json or json_lines
         joined by a '\n'
-        '''
+        """
         valid_input = self.validate_input()
         if not valid_input:
             err_json = {}
-            err_json['message'] = "Please enter valid query params. Fields must exist for the given project. If not sure, please access http://mydigurl/projects/<project_name>/fields API for reference"
+            err_json[
+                'message'] = "Please enter valid query params. Fields must exist for the given project. If not sure, please access http://mydigurl/projects/<project_name>/fields API for reference"
             return rest.bad_request(err_json)
 
         resp = self.cquery.process()[0]
@@ -55,18 +55,19 @@ class EventQueryProcessor(object):
             if len(resp['hits']['hits']) > 0 and 'doc_id' in resp['hits']['hits'][0]['_source'].keys():
                 docid = resp['hits']['hits'][0]['_source']['doc_id']
                 argmap = {}
-                argmap['measure/value'] = docid 
+                argmap['measure/value'] = docid
                 argmap['_group-by'] = 'event_date'
                 argmap['_interval'] = self.agg
                 self.request.args = argmap
-                newquery = ConjunctiveQueryProcessor(self.request, self.project_name, self.config, self.project_root_name, self.es)
+                newquery = ConjunctiveQueryProcessor(self.request, self.project_name, self.config,
+                                                     self.project_root_name, self.es)
                 resp = newquery.process()
                 if resp[1] == 400:
                     return resp
                 else:
                     resp = resp[0]
                 logger.debug("Response for query is {}".format(resp))
-                ts,dims = DigOutputProcessor(resp['aggregations'][self.field],self.agg_field).process()
+                ts, dims = DigOutputProcessor(resp['aggregations'][self.field], self.agg_field).process()
                 ts_obj = TimeSeries(ts, dict(), dims).to_dict()
                 return rest.ok(ts_obj)
             else:
@@ -76,15 +77,16 @@ class EventQueryProcessor(object):
             return rest.bad_request("Enter valid query")
 
     def process_event_query(self):
-        '''
+        """
         This is the main function in this class. This calls several functions to validate input, set match clauses, set filter clauses
         set sort clauses and finally resolve any nested documents if needed. Finally this function returns the data as a json or json_lines
         joined by a '\n'
-        '''
+        """
         valid_input = self.validate_input()
         if not valid_input:
             err_json = {}
-            err_json['message'] = "Please enter valid query params. Fields must exist for the given project. If not sure, please access http://mydigurl/projects/<project_name>/fields API for reference"
+            err_json[
+                'message'] = "Please enter valid query params. Fields must exist for the given project. If not sure, please access http://mydigurl/projects/<project_name>/fields API for reference"
             return rest.bad_request(err_json)
         try:
             resp = self.cquery.process()
@@ -95,10 +97,10 @@ class EventQueryProcessor(object):
                 resp = resp[0]
             if resp is not None and len(resp['aggregations'][self.field]['buckets']) > 0:
                 if "." not in self.field and self.config[self.field]['type'] == "date":
-                    ts,dims = DigOutputProcessor(resp['aggregations'][self.field],self.agg_field,True).process()
+                    ts, dims = DigOutputProcessor(resp['aggregations'][self.field], self.agg_field, True).process()
                     ts_obj = TimeSeries(ts, {}, dims).to_dict()
                 else:
-                    ts,dims = DigOutputProcessor(resp['aggregations'][self.field],self.agg_field,False).process()
+                    ts, dims = DigOutputProcessor(resp['aggregations'][self.field], self.agg_field, False).process()
                     ts_obj = TimeSeries(ts, {}, dims).to_dict()
                 return rest.ok(ts_obj)
             else:
@@ -108,7 +110,7 @@ class EventQueryProcessor(object):
             return rest.internal_error("Internal Error occured")
 
     def validate_input(self):
-        field = self.request.args.get('_group-by',None)
+        field = self.request.args.get('_group-by', None)
         if field is None:
             return False
         elif "." not in field and field not in self.config.keys():
