52c16f966c0c265156d70d2383b5089613502b8c
Amandeep Singh <amandeep.s.saggu@gmail.com>
2018-04-20 13:35:00 -0700

    if the value is zero it is not None

--- ws/search/tests/test_impute_values.py
+++ ws/search/tests/test_impute_values.py
@@ -14,14 +14,16 @@ class TestImputeTSValues(unittest.TestCase):
         ["2012-12-01T00:00:00.000Z",1,472.5],["2013-01-01T00:00:00.000Z",1,121.6],
         ["2013-02-01T00:00:00.000Z",null],["2013-03-01T00:00:00.000Z",1,129.9],
         ["2013-04-01T00:00:00.000Z",1,127.2],["2013-05-01T00:00:00.000Z",1,127],["2013-06-01T00:00:00.000Z",1,122.3],
-        ["2013-07-01T00:00:00.000Z",1,119.8],["2013-08-01T00:00:00.000Z",1,119.1]]"""
+        ["2013-07-01T00:00:00.000Z",1,119.8],["2013-08-01T00:00:00.000Z",1,119.1], ["2013-09-01T00:00:00.000Z",1,0], 
+        ["2013-10-01T00:00:00.000Z",1,23]]"""
         self.ts_obj1 = TimeSeries(json.loads(str_ts1), None, None, impute_method='previous')
         self.ts_obj2 = TimeSeries(json.loads(str_ts1), None, None, impute_method='average')
 
     def test_impute_values_previous(self):
         ts = self.ts_obj1.ts
-        self.assertTrue(len(ts) == 12)
+        self.assertTrue(len(ts) == 14)
         self.assertEqual(ts[5][len(ts[5]) - 1], 121.6)
+        self.assertEqual(ts[12][len(ts[12]) - 1], 0)
 
     def test_impute_values_average(self):
         ts = self.ts_obj2.ts
