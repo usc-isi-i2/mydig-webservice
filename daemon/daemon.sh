36aa7080c490f2eb674c8f828687264057d5906c
greatyyx <bigyyx@gmail.com>
2018-06-28 10:44:41 -0700

    by default, stop all daemon processes

--- daemon/daemon.sh
+++ daemon/daemon.sh
@@ -7,15 +7,15 @@ echo "killing daemon process (if exists)"
 ps -ef | grep "dummy-this-is-mydig-daemon" | awk '{print $2}' | xargs kill -9
 
 if [ ${restart} != "no" ]; then
-    echo "starting daemon (ETK spaCy)"
-    python -u etk_spacy.py --dummy-this-is-mydig-daemon &
+#    echo "starting daemon (ETK spaCy)"
+#    python -u etk_spacy.py --dummy-this-is-mydig-daemon &
 #    echo "starting daemon (ACHE)"
 #    python -u ache_consumer.py --dummy-this-is-mydig-daemon &
-    echo "starting daemon (RSS Feed Crawler)"
-    python -u rss_consumer.py --dummy-this-is-mydig-daemon &
-
-    echo "starting daemon (External Crawler)"
-    python -u external_crawler_consumer.py --dummy-this-is-mydig-daemon &
+#    echo "starting daemon (RSS Feed Crawler)"
+#    python -u rss_consumer.py --dummy-this-is-mydig-daemon &
+#
+#    echo "starting daemon (External Crawler)"
+#    python -u external_crawler_consumer.py --dummy-this-is-mydig-daemon &
 fi
 
 echo "done"
