d28deda7bdeec7c609e0be2b2173c58d04f25f32
Amandeep Singh <amandeep.s.saggu@gmail.com>
2018-07-17 11:32:48 -0700

    remove multiple build args for docker

--- release_docker.sh
+++ release_docker.sh
@@ -6,9 +6,7 @@ source ./VERSION
 
 if [ "$opt" == "build" ]; then
     echo "building image..."
-    docker build --build-arg ETK_VERSION=${ETK_VERSION} \
-    --build-arg SPACY_UI_VERSION=${SPACY_UI_VERSION} \
-    -t uscisii2/mydig_ws:${MYDIG_WS_VERSION} .
+    docker build --build-arg ETK_VERSION=${ETK_VERSION}  -t uscisii2/mydig_ws:${MYDIG_WS_VERSION} .
 elif [ "$opt" == "push" ]; then
     echo "pushing image..."
     docker push uscisii2/mydig_ws:${MYDIG_WS_VERSION}
