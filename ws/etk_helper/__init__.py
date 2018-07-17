345905065a1838bc2d126876595c3dd791cc0677
Amandeep Singh <amandeep.s.saggu@gmail.com>
2018-07-17 11:37:19 -0700

    additional import statements to support variable replacement

--- ws/etk_helper/__init__.py
+++ ws/etk_helper/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from .em_base_generator import EmBaseGenerator
+from .em_additional_em_helper import *
 
 ebg = EmBaseGenerator(os.path.join(os.path.dirname(__file__), 'template.tpl'))
 
