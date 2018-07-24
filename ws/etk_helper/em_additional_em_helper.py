d469987b82a5fda1aa74429f2f6bb984678eed82
Amandeep Singh <amandeep.s.saggu@gmail.com>
2018-07-23 17:05:14 -0700

    relative paths for SPACY Rules and Inferlink rules

--- ws/etk_helper/em_additional_em_helper.py
+++ ws/etk_helper/em_additional_em_helper.py
@@ -6,22 +6,29 @@ def generated_additional_ems(additional_ems_path, generated_additional_ems_path,
                              glossary_dir, inferlink_dir, working_dir, spacy_dir):
     """
     This function will read the user defined etk modules and replace any paths.
-    :return:
 
+    Variables to replace:
+    # ${GLOSSARY_PATH}
+    # ${INFERLINK_RULES_PATH}
+    # ${SPACY_RULES_PATH}
+    :return:
+    the ETK modules with variables replaced by
 
     """
-    # Variables to replace:
-    # ${GLOSSARY_PATH}
+    #
 
     for additional_em_path in glob.glob('{}/em_*.py'.format(additional_ems_path)):
         f_in = open(additional_em_path)
         f = f_in.read()
-        f = replace_variables(f, glossary_dir)
+        f = replace_variables(f, glossary_dir, inferlink_dir, spacy_dir)
         f_out = open('{}/{}'.format(generated_additional_ems_path, os.path.basename(additional_em_path)), 'w')
         f_out.write(f)
         f_in.close()
         f_out.close()
 
 
-def replace_variables(content, glossary_dir):
-    return content.replace("${GLOSSARY_PATH}", glossary_dir)
+def replace_variables(content, glossary_dir, inferlink_dir, spacy_dir):
+    content = content.replace("${GLOSSARY_PATH}", glossary_dir)
+    content = content.replace("${SPACY_RULES_PATH}", spacy_dir)
+    content = content.replace("${INFERLINK_RULES_PATH}", inferlink_dir)
+    return content
