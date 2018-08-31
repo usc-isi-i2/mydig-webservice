589ea2dfc8439193dc09dbf2499b2f982868b9da
Amandeep Singh <amandeep.s.saggu@gmail.com>
2018-08-31 11:24:41 -0700

    use a new connection every time

--- ws/hbase/hbase_adapter.py
+++ ws/hbase/hbase_adapter.py
@@ -14,28 +14,26 @@ class HBaseAdapter(object):
     </property>
     """
 
-    def __init__(self, host='hbase', connection_pool_size=10, timeout=6000000):
+    def __init__(self, host='hbase', connection_pool_size=10, timeout=60000000):
         self._conn_host = host
-        self._conn_size = connection_pool_size
         self._timeout = timeout
-        self._conn_pool = happybase.ConnectionPool(size=self._conn_size,
-                                                   host=self._conn_host,
-                                                   timeout=self._timeout)
-
-        self._tables = {}
 
     def tables(self):
-        with self._conn_pool.connection() as _conn:
-            try:
-                return _conn.client.getTableNames()
-            except Exception as e:
-                print('Exception: {}, while trying to get a list of tables'.format(str(e)))
-                return _conn.client.getTableNames()
+        c = happybase.Connection(host=self._conn_host, timeout=self._timeout)
+        try:
+            return c.client.getTableNames()
+        except Exception as e:
+            print('Exception: {}, while trying to get a list of tables'.format(str(e)))
+            return list()
 
     def create_table(self, table_name, family_name):
         if not bytes(table_name, encoding='utf-8') in self.tables():
-            with self._conn_pool.connection() as _conn:
-                _conn.create_table(table_name, {family_name: dict()})
+            c = happybase.Connection(host=self._conn_host, timeout=self._timeout)
+            try:
+                c.create_table(table_name, {family_name: dict()})
+            except Exception as e:
+                print('Failed to create table: {}, trying once more'.format(table_name))
+                c.create_table(table_name, {family_name: dict()})
 
     def get_record(self, record_id, table_name, column_names, column_family):
         try:
@@ -53,10 +51,8 @@ class HBaseAdapter(object):
 
     def get_table(self, table_name):
         try:
-            if table_name not in self._tables:
-                with self._conn_pool.connection() as _conn:
-                    self._tables[table_name] = _conn.table(table_name)
-            return self._tables[table_name]
+            c = happybase.Connection(host=self._conn_host, timeout=self._timeout)
+            return c.table(table_name)
         except Exception as e:
             print('Exception:{}, while retrieving table: {}'.format(e, table_name))
         return None
@@ -87,7 +83,6 @@ class HBaseAdapter(object):
         batch.send()
 
     def delete_table(self, table_name):
-        table_name = bytes(table_name, encoding='utf-8')
-        with self._conn_pool.connection() as _conn:
-            print(table_name)
-            _conn.delete_table(table_name, disable=True)
+        c = happybase.Connection(host=self._conn_host, timeout=self._timeout)
+
+        c.delete_table(table_name, disable=True)
